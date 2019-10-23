8b84e31f254bf081a38309da0975ac9774cb97f5
ian-sanchez <silkyhairbear@gmail.com>
2019-10-23 11:03:21 -0700

    finished endpoints

--- user/user-router.js
+++ user/user-router.js
@@ -39,10 +39,51 @@ router.get('/nanny/:id', (req, res) => {
             res.status(500).json({ error: 'Error getting User' })
         })
 })
+//delete nanny
+router.delete('/nanny/:id', (req, res) => {
+    const { id } = req.params;
+    Users.removeNanny(id)
+        .then(deleted => {
+            if(deleted) {
+                res.status(204).end()
+            } else {
+                res.status(404).json({ error: 'User with Id does not exist' })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: 'Error deleting User' })
+        })
+})
+//update nanny
+router.put('/nanny/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, email, zip_code, availability_start, availability_end } = req.body;
+    if(!name && !email && !zip_code && !availability_start && !availability_end) {
+        res.status(400).json({ error: 'Required Feilds not met'})
+    }
+    Users.updateParent(id, { name, email, zip_code, availability_start, availability_end })
+        .then(updated => {
+            if(updated) {
+                Users.findByParentId(id)
+                    .then(user => res.status(200).json(user))
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).json({ error: 'Error getting user' })
+                    })
+            } else {
+                res.status(404).json({ error: 'User with Id not Found' })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: 'Error updateing user' })
+        })
+})
 //post parent
 router.post('/parent', (req, res) => {
-    const { name, kids, email, zip_code, selected, user_id } = req.body;
-    Users.insertParent({ name, kids, email, zip_code, selected, user_id })
+    const { name, kids, email, zip_code } = req.body;
+    Users.insertParent({ name, kids, email, zip_code })
         .then(id => {
             res.status(200).json({ message: 'Signup Succesful', id })
         })
@@ -76,6 +117,47 @@ router.get('/parent/:id', (req, res) => {
         .catch(err => {
             console.log(err)
             res.status(500).json({ error: 'Error getting User' })
+        })
+})
+//delete parent
+router.delete('/parent/:id', (req, res) => {
+    const { id } = req.params;
+    Users.removeParent(id)
+        .then(deleted => {
+            if(deleted) {
+                res.status(204).end()
+            } else {
+                res.status(404).json({ error: 'User with Id does not exist' })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: 'Error deleting User' })
+        })
+})
+//update parent
+router.put('/parent/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, kids, email, zip_code } = req.body;
+    if(!name && !kids && !email && !zip_code) {
+        res.status(400).json({ error: 'Required Feilds not met'})
+    }
+    Users.updateParent(id, { name, kids, email, zip_code })
+        .then(updated => {
+            if(updated) {
+                Users.findByNannyId(id)
+                    .then(user => res.status(200).json(user))
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).json({ error: 'Error getting user' })
+                    })
+            } else {
+                res.status(404).json({ error: 'User with Id not Found' })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: 'Error updateing user' })
         })
 })
 
